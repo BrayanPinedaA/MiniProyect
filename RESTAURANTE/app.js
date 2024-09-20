74d91bf2a6958fd72521e3be55ad8aaae41ad83c
BrayanP <enderyt588@gmail.com>
2024-09-20 13:27:10 -0500

    pro

--- RESTAURANTE/app.js
+++ RESTAURANTE/app.js
@@ -2,16 +2,17 @@ const express=require('express')
 const bodyParser=require('body-parser')
 const mysql2=require('mysql2/promise')
 const path = require('path'); 
+const { Console } = require('console');
 
 const app=express()
 
 app.use(bodyParser.urlencoded({extended:true})) //leer url
 app.use(bodyParser.json()) //converit los datos en json
 
-app.use(express.static(path.join(__dirname, 'RESTAURANTE')));
+app.use(express.static(path.join(__dirname, 'Public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'RESTAURANTE/pagina/index.html'));
+    res.sendFile(path.join(__dirname, 'Public/pagina/index.html'));
 });
 
 const db=mysql2.createPool({
@@ -25,12 +26,15 @@ app.post('/insertar',async(req,res)=>{
 
     try{
     const [rows]=await db.query('INSERT INTO Ordenes (Num_Orden, Fecha, Hora_Cobro, Mesa, Propina, Mesero_Fk, Cliente_Fk) VALUES (?,?,?,?,?,?,?)',[Num_Orden, Fecha, Hora_Cobro, Mesa, Propina, Mesero_Fk, Cliente_Fk])
+    const orden_id=rows.insertId;
 
     console.log(rows)
-    /*  res.send('Mi perro se insertaron los datos')  */
-     res.redirect('/pagina/Facturas.html'); 
-    
+
+    res.json({
+            NumeroPedido: orden_id
+    }).redirect('/pagina/index.html');
     }
+
     catch(error)
     { 
         console.error('El error empieza aqui ',error)
